9e218dc77f2b80a15dd0d9fdf80e26a958565a6b
Jackybenzaquen <jackybenzaquen@gmail.com>
2025-05-11 17:42:05 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => (
+  <div className="text-center mt-20 text-2xl font-bold text-blue-700">
+    Plataforma Sábanas Optimizadas
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
